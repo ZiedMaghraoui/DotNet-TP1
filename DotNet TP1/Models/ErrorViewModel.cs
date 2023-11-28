7a0dcdf9459ec795448305581bf6089dcf364e8f
Zied <ziedmaghraoui58@gmail.com>
2023-11-28 17:31:29 +0100

    Ajoutez des fichiers projet.

---
+++ DotNet TP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet_TP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
