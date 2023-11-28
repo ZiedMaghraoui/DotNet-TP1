8a5f21c09eb8b993093f642ff91035110a07c8e0
Zied <ziedmaghraoui58@gmail.com>
2023-11-28 20:01:57 +0100

    customer details by id

---
+++ DotNet TP1/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using DotNet_TP1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_TP1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            List<Customer> customers = new List<Customer>(){
+                new Customer{Id=0,Name="John"},
+                new Customer{Id=1, Name="Alex"},
+            };
+            Customer customer = customers.Find(c => c.Id == id);
+            return Content("id : " + id + "\tname: " + customer?.Name);
+        }
+    }
+}
