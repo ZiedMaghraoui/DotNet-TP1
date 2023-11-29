29f95d1b0d966c3558ba7fb53365e4315a90c7f2
Zied <ziedmaghraoui58@gmail.com>
2023-11-29 18:48:28 +0100

    set up db connexion and init migration

---
+++ DotNet TP1/Models/ApplicationdbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_TP1.Models
+{
+    public class ApplicationdbContext : DbContext
+    {
+        public ApplicationdbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie>? movies { get; set; }
+        public DbSet<Genre> genres { get; set; }
+    }
+}
--- DotNet TP1/Program.cs
+++ DotNet TP1/Program.cs
@@ -1,7 +1,14 @@
+using DotNet_TP1.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationdbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
