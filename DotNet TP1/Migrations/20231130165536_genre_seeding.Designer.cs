6c7a6d1df7ff6eb569d3f1686aba910522bf5da0
Zied <ziedmaghraoui58@gmail.com>
2023-11-30 18:00:48 +0100

    genre seed migration

--- DotNet TP1/Migrations/20231130165536_genre_seeding.Designer.cs
+++ DotNet TP1/Migrations/20231130165536_genre_seeding.Designer.cs
@@ -4,6 +4,7 @@ using DotNet_TP1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNet_TP1.Migrations
 {
     [DbContext(typeof(ApplicationdbContext))]
-    partial class ApplicationdbContextModelSnapshot : ModelSnapshot
+    [Migration("20231130165536_genre_seeding")]
+    partial class genre_seeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
