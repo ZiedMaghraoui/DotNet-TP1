9b93f8dbd39126650fcfacabc282165de0e12a48
Zied <ziedmaghraoui58@gmail.com>
2023-11-30 15:55:22 +0100

    genre-movies relation

--- DotNet TP1/Models/Genre.cs
+++ DotNet TP1/Models/Genre.cs
@@ -3,6 +3,7 @@
     public class Genre
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Movie> Movies { get; set; }
     }
 }
--- DotNet TP1/Models/Movie.cs
+++ DotNet TP1/Models/Movie.cs
@@ -1,8 +1,12 @@
-﻿namespace DotNet_TP1.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNet_TP1.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public Guid GenreId { get; set; }
+        public Genre Genre { get; set; }
     }
 }
